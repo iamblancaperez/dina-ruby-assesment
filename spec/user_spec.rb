1a7e7f935f409e5b4fdcbb8b24895b9790f18a84
Blanca Perez <blankp@gmail.com>
2017-09-20 12:26:40 -0300

    Testing the new user / Added migration of users table

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,7 +3,12 @@ require 'spec_helper'
 describe User do
 
 	before :each do
-	  @user = User.new "Name"
+	  @user = User.new
 	end
- 
+
+	describe "#new" do
+    it "takes one parameters and returns an User" do
+      @user.should be_an_instance_of User
+    end
+	end 
 end
