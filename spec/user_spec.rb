4f16ad37c86fb53ef93892a9acf68842a1901d79
Blanca Perez <blankp@gmail.com>
2017-09-20 12:33:08 -0300

    It should require a user name

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,20 +1,14 @@
 require 'spec_helper'
 
 describe User do
-
-	before :each do
-	  @user = User.new
-	end
-
-	describe "#new" do
-    it "takes one parameters and returns an User" do
-      @user.should be_an_instance_of User
-    end
-	end
-
 	describe "valid attributes" do
-		it "is valid with valid attributes"
-	  it "is not valid without a status"
-	  it "is not valid without a name"		
+		it "is valid with valid attributes" do
+	    expect(User.new).to be_valid
+	  end
+	  it "is not valid without a name" do
+		  user = User.new(name: nil)
+		  expect(user).to_not be_valid
+		end
+	  it "is not valid without a status"		
 	end
 end
