76c222b3d9fb96c3a628d377ad552f4914b461a9
Blanca Perez <blankp@gmail.com>
2017-09-20 12:39:47 -0300

    Added user status test

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,14 +1,22 @@
 require 'spec_helper'
 
 describe User do
-	describe "valid attributes" do
-		it "is valid with valid attributes" do
-	    expect(User.new(name: "UserName")).to be_valid
-	  end
-	  it "is not valid without a name" do
-		  user = User.new(name: nil)
-		  expect(user).to_not be_valid
-		end
-	  it "is not valid without a status"		
-	end
+
+  before :each do
+    @user = User.new(name: "UserName", status: "status")
+  end
+
+  describe "valid attributes" do
+    it "is valid with valid attributes" do
+      expect(@user).to be_valid
+    end
+    it "is not valid without a name" do
+      @user.name = nil
+      expect(@user).to_not be_valid
+    end
+    it "is not valid without a status" do
+      @user.status= nil
+      expect(@user).to_not be_valid
+    end   
+  end
 end
