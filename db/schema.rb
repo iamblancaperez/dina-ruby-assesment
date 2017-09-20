6298f97913dfba340134e2e1ff77c0199afb1090
Blanca Perez <blankp@gmail.com>
2017-09-20 12:46:15 -0300

    Added roles table to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920122429) do
+ActiveRecord::Schema.define(version: 20170920124448) do
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
